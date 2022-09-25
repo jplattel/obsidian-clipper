ffd9649a1206f84ea7bd7fb9b37efaa19ac08bc7
JP Etcheber <jetcheber@gmail.com>
2022-09-25 14:06:31 -0700

    feat: add domain to clipper

--- src/options.js
+++ src/options.js
@@ -1,3 +1,18 @@
+// Collapsible lists
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 // Saves options to chrome.storage
 function saveOptions() {
